1cbdaaad3b0735ecb264616bb8e70fae572ce90a
NilGadhiya20 <Neel@DESKTOP-REC10H8>
2026-04-06 08:37:40 +0530

    Add framer motion page transitions

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import AnimatedPage from './components/AnimatedPage';
+
+// Pages
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -12,22 +16,32 @@ import Cart from './pages/Cart';
 import RestaurantDetail from './pages/RestaurantDetail';
 import OrderDetail from './pages/OrderDetail';
 
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<AnimatedPage><Home /></AnimatedPage>} />
+        <Route path="/about" element={<AnimatedPage><About /></AnimatedPage>} />
+        <Route path="/contact" element={<AnimatedPage><Contact /></AnimatedPage>} />
+        <Route path="/login" element={<AnimatedPage><Login /></AnimatedPage>} />
+        <Route path="/register" element={<AnimatedPage><Register /></AnimatedPage>} />
+        <Route path="/cart" element={<AnimatedPage><Cart /></AnimatedPage>} />
+        <Route path="/restaurant/:id" element={<AnimatedPage><RestaurantDetail /></AnimatedPage>} />
+        <Route path="/order/:id" element={<AnimatedPage><OrderDetail /></AnimatedPage>} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 export default function App() {
   return (
     <AuthProvider>
       <CartProvider>
         <BrowserRouter>
           <Navbar />
-          <Routes>
-            <Route path="/"                  element={<Home />} />
-            <Route path="/about"             element={<About />} />
-            <Route path="/contact"           element={<Contact />} />
-            <Route path="/login"             element={<Login />} />
-            <Route path="/register"          element={<Register />} />
-            <Route path="/cart"              element={<Cart />} />
-            <Route path="/restaurant/:id"   element={<RestaurantDetail />} />
-            <Route path="/order/:id"        element={<OrderDetail />} />
-          </Routes>
+          <AnimatedRoutes />
           <Footer />
         </BrowserRouter>
       </CartProvider>
